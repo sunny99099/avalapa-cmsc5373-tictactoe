920e2b20e683e4e4f36e61e90416e7be2aadc42c
avalapa <avalapa@uco.edu>
2025-01-31 16:59:19 -0600

    start

---
+++ public/controller/firebase_core.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCU-n1q7C8gPszzIvZBMO0y3-Dw8pS2Sgc",
+  authDomain: "avalapa-cmsc5373-webapp.firebaseapp.com",
+  projectId: "avalapa-cmsc5373-webapp",
+  storageBucket: "avalapa-cmsc5373-webapp.firebasestorage.app",
+  messagingSenderId: "417093626321",
+  appId: "1:417093626321:web:cb94d5b55235131c6598f0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
