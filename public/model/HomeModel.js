29b8a0a3a7ff5c1df03b0b5efc7df7d9d3b56665
avalapa <avalapa@uco.edu>
2025-02-07 19:10:55 -0600

    define model

--- public/model/HomeModel.js
+++ public/model/HomeModel.js
@@ -1,2 +1,23 @@
+export const marking = {
+    X: 'X', O: 'O', U: 'U'
+}
+
+export const images = {
+    X: '/images/X.png',
+    U: '/images/U.png',
+    O: '/images/O.png'
+}
+
+export const GameState = {
+    INIT: 0, PLAYING: 1, DONE: 2
+}
+
+export const GamePlayStrategy = {
+    VS_HUMAN: 'vsHuman', VS_RANDOM: 'vsRandom'
+}
+
 export class HomeModel {
+    reset(){
+
+    }
 }
